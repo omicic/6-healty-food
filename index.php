d22beafae2cbc658e917a6f1afc39ab9f34ac882
olivera <omicic@gmail.com>
2021-03-28 18:19:56 +0200

    home

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php include 'header.php'?>
 
-<header class="mainHeader"> 
+<header class="mainHeader">
     <div>
         <h1>Odaberite zdravlje</h1>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit accusantium nulla nesciunt vitae
@@ -9,5 +9,23 @@
         <button>Procitajte vise</button>
     </div>
 </header>
+<section class="uvod">
+    <article>
+        <h2>Koraci do <span>zdravlja</span></h2>
+        <h3>Prvi korak</h3>
+        <h4>Dole ugljeni hidrati</h4>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error animi architecto eveniet est magnam beatae
+            assumenda numquam suscipit repudiandae, deserunt nemo placeat unde iste facere autem. Dolore quia incidunt
+            a.</p>
+    </article>
+    <article>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque suscipit porro ad accusamus cupiditate
+            magni, consequuntur dolores sapiente, quod sit reiciendis minus eius iusto officiis excepturi. Repellat
+            voluptatem consectetur numquam?</p>
+            <h3>02</h3>
+            <h4>Gore proteini</h4>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente hic quidem dolore nisi quisquam dicta, dolorem, qui libero excepturi incidunt ut quibusdam reiciendis. Voluptatem architecto quo impedit voluptates. Officiis nam cumque totam illo repellat commodi ex animi corrupti harum non!</p>
+    </article>
+</section>
 
 <?php include 'footer.php'?>
